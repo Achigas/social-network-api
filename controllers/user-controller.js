d025dbd41db99291dc3dd1503a3f78abb1fb0189
AChigas <Achigas34@gmail.com>
2020-09-16 19:03:14 -0700

    changed friend function routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,6 +4,14 @@ const userController = {
     //get all users
     getAllUsers(req,res) {
         User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+         })
+         .populate ({
+             path: 'friends',
+             select: '-__v'
+         })
         .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -18,6 +26,10 @@ const userController = {
            .populate({
                path: 'thoughts',
                select: '-__v'
+            })
+            .populate ({
+                path: 'friends',
+                select: '-__v'
             })
            .select('-__v')
            .then(dbUserData => res.json(dbUserData))
